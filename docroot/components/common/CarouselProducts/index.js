9219d3c560d8ad414c46a5b86c9d9217c7cee751
Irene <irene.borada@rocket-path.com>
2022-07-21 10:23:42 +0000

    Fix: Removes product_temp that broke the pipeline

--- docroot/components/common/CarouselProducts/index.js
+++ docroot/components/common/CarouselProducts/index.js
@@ -4,6 +4,7 @@ import Container from '../Container';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import FeaturedProduct from '../FeaturedProduct';
 
 const CarouselProducts = ({ title, items, cta }) => {
     const settings = {
@@ -43,8 +44,8 @@ const CarouselProducts = ({ title, items, cta }) => {
                 <Slider {...settings} className="products-slider">
                     {items?.map((item, index) => {
                         return (
-                            <Box p="0 5px" key={`carousel-product-${index}`} border="1px solid">
-                                <div>Product teaser</div>
+                            <Box p="0 5px" key={`carousel-product-${index}`}>
+                                <FeaturedProduct />
                             </Box>
                         );
                     })}
--- docroot/pages/product_temp/index.js
+++
@@ -1,5 +0,0 @@
-import ProductView from 'components/product/ProductView';
-
-export default function ProductTemp() {
-    return <ProductView />;
-}
